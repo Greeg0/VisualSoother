4e829cbbca484fe6c82fc084a85f5a7892a8d676
Duckfeld-McJimmers <ivan.desc@gmail.com>
2021-12-16 19:14:56 -0800

    well i worked on it and made progress i guess...

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 function love.load()
-	rectw=100
-	recth=100
 	winx = 800
 	winy = 600
 	love.window.setMode(winx, winy)
@@ -8,7 +6,32 @@ function love.load()
 	posy = 300
 	x = 200
 	y = x
+	co = 1
+	dvdImg=love.graphics.newImage('dvd.png')
+	dvd={}
+	dvd.image=dvdImg
+	dvd.height=dvdImg:getHeight()
+	dvd.width=dvdImg:getWidth()
+	
+	colour={ 
+		{0.5, 0, 0} ,--Maroon
+		{0, 0.5, 0} ,--Green
+		{0.5, 0.5, 0} ,--Olive
+		{0, 0, 0.5} ,--Royal
+		{0.5, 0, 0.5} ,--Purple
+		{0, 0.5, 0.5} ,--Teal
+		{0.5, 0.5, 0.5} ,--Gray
+		{1, 0, 0} ,--Red
+		{0, 1, 0} ,--Lime
+		{1, 1, 0} ,--Yellow
+		{0.75, 0.75, 0.75} ,--Silver
+		{0, 0, 1} ,--Blue
+		{1, 0, 1} ,--Pink
+		{0, 1, 1} ,--Cyan
+		{1, 1, 1} --White
+		}	
 end
+
 
 
 function collides(pos, win, rect)
@@ -17,11 +40,17 @@ end
 
 
 
-function push(a, pos, win, rect) --for the fix of delta time messing with collision
-	if a>0 then
-		pos=0 else
+function push(a, pos, win, rect) --for the fix of delta time messing with collision, now also includes the colour advance code.
+		
+		if co==15 then co=0 end
+		co=co+1
+		
+		if a>0 then
+			pos=0 else
 			pos=win-rect
+			
 	end
+	return pos
 end
 
 
@@ -30,33 +59,31 @@ end
 
 function love.update(dt)
     
-    if collides(posx, winx, rectw) then 
-    	x=-x
-    	push(x, posx, winx, rectw)
-    end
     
-   -- while collides(posx, winx) do --sometimes reversing variable is not enough to resolve this due to delta time.
-   -- posx = posx + x*dt
-   -- end
-
     posx = posx + x*dt
-		
-    if collides(posy, winy, recth) then 
-    	y=-y
-    	push(y, posy, winy, recth)
+    
+    if collides(posx, winx, dvd.width) then 
+    	x=-x
+    	posx=push(x, posx, winx, dvd.width)
     end
     
- --   while collides(posy, winy) do --sometimes reversing variable is not enough to resolve this due to delta time.
- --   posy = posy + y*dt
- --   end
-	
+
+
     posy = posy + y*dt
-     
-print(posx, posy)
+		
+    if collides(posy, winy, dvd.height) then 
+    	y=-y
+    	posy=push(y, posy, winy, dvd.height)
+    end
+    
+
+    
+    
 end
 
 
 
 function love.draw()
-	love.graphics.rectangle('fill', posx, posy, 100, 100)
+	love.graphics.setColor(colour[co][1], colour[co][2], colour[co][3])
+	love.graphics.draw(dvd.image, posx, posy, 0)
 end
